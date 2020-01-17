5c93cacf874df72f490ef7f4f180fedb5142d0ca
Dzmitry Furmanau <amator228@gmail.com>
2020-01-17 19:16:43 +0300

    add README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+##### Java 8
+##### 1. Spring IoC
+##### 2. Spring Data JPA
+##### 3. Spring REST
+##### 4. Spring Security
+##### 5. Hibernate
+##### 6. DTO
+##### 7. Jackson
+##### 8. Maven
+##### 9. H2DataBase
+##### 10. Logging
+##### 11. Localization
+##### 12. Exception Handling
+##### 13. Validation
+##### 14. Dozer
+##### 15. Swagger
--- src/main/java/com/onlinestore/app/repository/CustomerRepository.java
+++ src/main/java/com/onlinestore/app/repository/CustomerRepository.java
@@ -2,24 +2,11 @@ package com.onlinestore.app.repository;
 
 import com.onlinestore.app.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 /**
  * The interface Customer repository.
  */
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-    /**
-     * Find by name starts with param list.
-     *
-     * @param name the name
-     * @return the list
-     */
-    @Query(value = "SELECT u FROM Customer u WHERE LOWER(u.name) LIKE LOWER(CONCAT('%',:name))")
-    List<Customer> findByNameStartsWithParam(@Param("name") String name);
 
     /**
      * Exists by name boolean.
